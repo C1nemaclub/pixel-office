d23041140f56e4e4a9da4ab35a7ce668f7cfbdd8
C1nemaclub <santiagov801@gmail.com>
2023-07-28 22:47:14 -0500

    style: avatar selection modal

--- client/src/components/JoinModal/JoinModal.tsx
+++ client/src/components/JoinModal/JoinModal.tsx
@@ -1,5 +1,6 @@
-import { FaLaptopCode,FaArrowLeft } from 'react-icons/fa';
-import { MODAL_TYPES } from '../../App';
+import {FaArrowLeft,FaArrowRight } from 'react-icons/fa';
+import { charsImgArray } from '../../utils/contants';
+import { useState } from 'react';
 
 
 type JoinModalProps = {
@@ -8,14 +9,27 @@ type JoinModalProps = {
 };
 
 function JoinModal({ onJoin, onReturn }: JoinModalProps) {
+  const [activeChar, setActiveChar] = useState<number>(0) 
   return (
-    <div className='py-12 px-6 max-w-[400px] flex flex-col justify-center items-center gap-4 bg-[#110c27] absolute-center'>
+    <div className='py-12 px-6 w-full min-w-[300px] max-w-[400px] flex flex-col justify-center items-center gap-4 bg-[#110c27] absolute-center'>
       {/* <FaArrowLeft onClick={()=> onReturn(MODAL_TYPES.MAIN)} size={30} color="#fff"  /> */}
       <h1 className='text-3xl font-bold text-center text-slate-50'>
-        Welcome to PiXel Office
+        Pick your Avatar
       </h1>
-      <FaLaptopCode size={70} color='#fff' />
-      <input  />
+      <div className="flex items-center gap-2">
+        <FaArrowLeft  size={45} color="#fff" className="cursor-pointer" onClick={()=> setActiveChar(prev => prev - 1)}/>
+      <div className="w-[150px] overflow-hidden select-none">
+        <div className="w-[1200px] flex justify-center align-center transition-all " style={{transform: `translateX(${activeChar * (-150)}px)`}}>
+        {charsImgArray.map((url: string)=>{
+          return <div className="w-[150px]"> 
+            <img alt="character" src={url} className="w-full h-full object-cover"  />
+            </div>
+        })}
+          </div>
+        </div>
+        <FaArrowRight size={45} color="#fff" className="cursor-pointer" onClick={()=> setActiveChar(prev => prev + 1)} />
+      </div>
+      <input className="custom-input w-full outline-none" placeholder='Name'/>
       <button onClick={onJoin} className='btn w-full'>Join Office</button>
     </div>
   );
---
+++ client/src/utils/contants.ts
@@ -0,0 +1,10 @@
+import char1 from '../assets/chars/char1.png';
+import char2 from '../assets/chars/char2.png';
+import char3 from '../assets/chars/char3.png';
+import char4 from '../assets/chars/char4.png';
+import char5 from '../assets/chars/char5.png';
+import char6 from '../assets/chars/char6.png';
+import char7 from '../assets/chars/char7.png';
+import char8 from '../assets/chars/char8.png';
+
+export const charsImgArray = [char1, char2, char3, char4, char5, char6, char7, char8];
