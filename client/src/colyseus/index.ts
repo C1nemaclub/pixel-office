a4630c4e9a0ea2fb08f39a6f3d63ffb1175813bb
C1nemaclub <santiagov801@gmail.com>
2023-08-05 19:20:31 -0500

    devSTart and start for backend

--- client/src/colyseus/index.ts
+++ client/src/colyseus/index.ts
@@ -1,6 +1,6 @@
 import * as Colyseus from 'colyseus.js';
 
 const environment = import.meta.env.VITE_NODE_ENV
-const serverURL = environment === 'production' ? 'https://magenta-lovebird-kit.cyclic.app/' : 'ws://localhost:2567'
+const serverURL = environment === 'production' ? 'https://magenta-lovebird-kit.cyclic.app/' : 'ws://localhost:8080'
 
 export const client = new Colyseus.Client(serverURL);
