ea51e2b62e8733affc229c54fdcae4e0f81abe3d
C1nemaclub <santiagov801@gmail.com>
2023-08-05 20:15:13 -0500

    production server url

--- client/src/colyseus/index.ts
+++ client/src/colyseus/index.ts
@@ -1,6 +1,6 @@
 import * as Colyseus from 'colyseus.js';
 
 const environment = import.meta.env.VITE_NODE_ENV
-const serverURL = environment === 'production' ? 'https://magenta-lovebird-kit.cyclic.app/' : 'ws://localhost:8080'
+const serverURL = environment === 'production' ? 'https://pixel-office-backend-60b13454427a.herokuapp.com/' : 'ws://localhost:2567'
 
 export const client = new Colyseus.Client(serverURL);
