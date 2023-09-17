c89520bc78221af7d8d319b727babc708b729008
C1nemaclub <santiagov801@gmail.com>
2023-09-16 21:36:01 -0500

    turn/stun server test config added

--- client/src/hooks/useCallManager.tsx
+++ client/src/hooks/useCallManager.tsx
@@ -69,6 +69,20 @@ const useCallManager = () => {
     const peer = new Peer({
       initiator: true,
       trickle: false,
+      config: {
+        iceServers: [
+          {
+            urls: 'stun:numb.viagenie.ca',
+            username: 'sultan1640@gmail.com',
+            credential: '98376683',
+          },
+          {
+            urls: 'turn:numb.viagenie.ca',
+            username: 'sultan1640@gmail.com',
+            credential: '98376683',
+          },
+        ],
+      },
       stream,
     });
 
